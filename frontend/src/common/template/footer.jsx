7608438b3fcf9087327608e5a8188b86193e7b65
igor daniel vasconcelos de lima <drakelima>
2020-04-20 17:16:55 -0300

    Aplicacao para ciclos de controle financeiro

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'> 
+        <strong> 
+            Copyright &copy; 2020
+            <a href='http://igordaniel.com.br' target='_blank'> Igor Daniel</a>.
+        </strong>
+    </footer>
+)
